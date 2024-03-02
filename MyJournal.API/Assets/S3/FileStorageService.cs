1ef5d333a7662dcc1ec4b12ed992d4b99143aebc
null <lesha.smirnov2019@mail.ru>
2024-03-02 18:14:54 +0300

    [=] now the attachments will have direct links to the file along with the ability to download it

--- MyJournal.API/Assets/S3/FileStorageService.cs
+++ MyJournal.API/Assets/S3/FileStorageService.cs
@@ -10,20 +10,22 @@ public sealed class FileStorageService(
 	private const string ColdBucketName = "myjournal_logs";
 	private const string HotBucketName = "myjournal_assets";
 
-	private async Task UploadAsync(string bucket, string key, Stream fileStream, CancellationToken cancellationToken = default(CancellationToken))
+	private async Task<string> UploadAsync(string bucket, string key, Stream fileStream, CancellationToken cancellationToken = default(CancellationToken))
 	{
-		_ = await amazonS3Client.PutObjectAsync(request: new PutObjectRequest()
+		PutObjectResponse response = await amazonS3Client.PutObjectAsync(request: new PutObjectRequest()
 		{
 			BucketName = bucket,
 			Key = key,
-			InputStream = fileStream
+			InputStream = fileStream,
+			CannedACL = S3CannedACL.PublicRead
 		}, cancellationToken: cancellationToken);
+		return $"https://{bucket}.hb.ru-msk.vkcs.cloud/{key}";
 	}
 
-	public async Task UploadLogAsync(string key, Stream fileStream, CancellationToken cancellationToken = default(CancellationToken))
+	public async Task<string> UploadLogAsync(string key, Stream fileStream, CancellationToken cancellationToken = default(CancellationToken))
 		=> await UploadAsync(bucket: ColdBucketName, key: key, fileStream: fileStream, cancellationToken: cancellationToken);
 
-	public async Task UploadFileAsync(string key, Stream fileStream, CancellationToken cancellationToken = default(CancellationToken))
+	public async Task<string> UploadFileAsync(string key, Stream fileStream, CancellationToken cancellationToken = default(CancellationToken))
 		=> await UploadAsync(bucket: HotBucketName, key: key, fileStream: fileStream, cancellationToken: cancellationToken);
 
 	public async Task<Stream> GetFileAsync(string key, CancellationToken cancellationToken = default(CancellationToken))
--- MyJournal.API/Assets/S3/IFileStorageService.cs
+++ MyJournal.API/Assets/S3/IFileStorageService.cs
@@ -2,8 +2,8 @@ namespace MyJournal.API.Assets.S3;
 
 public interface IFileStorageService
 {
-	Task UploadLogAsync(string key, Stream fileStream, CancellationToken cancellationToken = default(CancellationToken));
-	Task UploadFileAsync(string key, Stream fileStream, CancellationToken cancellationToken = default(CancellationToken));
+	Task<string> UploadLogAsync(string key, Stream fileStream, CancellationToken cancellationToken = default(CancellationToken));
+	Task<string> UploadFileAsync(string key, Stream fileStream, CancellationToken cancellationToken = default(CancellationToken));
 	Task<Stream> GetFileAsync(string key, CancellationToken cancellationToken = default(CancellationToken));
 	Task DeleteFileAsync(string key, CancellationToken cancellationToken = default(CancellationToken));
 }
