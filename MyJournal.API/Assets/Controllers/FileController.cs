4215808c9de51a7db4af9daf7a6658ad797f8390
null <lesha.smirnov2019@mail.ru>
2024-03-19 13:18:24 +0300

    [+] added documentation for FileController actions

--- MyJournal.API/Assets/Controllers/FileController.cs
+++ MyJournal.API/Assets/Controllers/FileController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 using MyJournal.API.Assets.DatabaseModels;
+using MyJournal.API.Assets.ExceptionHandlers;
 using MyJournal.API.Assets.S3;
 using MyJournal.API.Assets.Validation;
 using MyJournal.API.Assets.Validation.Validators;
@@ -29,7 +30,29 @@ public sealed class FileController(
 
 	#region Methods
 	#region GET
+	/// <summary>
+	/// Сохранить файл по ссылке
+	/// </summary>
+	/// <remarks>
+	/// <![CDATA[
+	/// Пример запроса к API:
+	///
+	///	GET api/file/download?Link=https%3A%2F%2Fmyjournal_assets.hb.ru-msk.vkcs.cloud%2Fabd3618c-2099-4738-bc9b-8bbc215090b9.jpg
+	///
+	/// Параметры:
+	///
+	///	Link - ссылка на файл для сохранения
+	///
+	/// ]]>
+	/// </remarks>
+	/// <response code="200">Чат отмечен как прочитанный</response>
+	/// <response code="400">Некорректные входные данные</response>
+	/// <response code="401">Пользователь не авторизован или авторизационный токен неверный</response>
 	[HttpGet(template: "download")]
+	[Produces(contentType: MediaTypeNames.Application.Json)]
+	[ProducesResponseType(statusCode: StatusCodes.Status200OK, type: typeof(void))]
+	[ProducesResponseType(statusCode: StatusCodes.Status400BadRequest, type: typeof(ErrorResponse))]
+	[ProducesResponseType(statusCode: StatusCodes.Status401Unauthorized, type: typeof(ErrorResponse))]
 	public async Task<ActionResult<FileLink>> DownloadFile(
 		[FromQuery] FileLink request,
 		CancellationToken cancellationToken = default(CancellationToken)
@@ -51,7 +74,30 @@ public sealed class FileController(
 	#endregion
 
 	#region PUT
+	/// <summary>
+	/// Сохранить файл на сервере
+	/// </summary>
+	/// <remarks>
+	/// <![CDATA[
+	/// Пример запроса к API:
+	///
+	///	PUT api/file/{bucket}/upload
+	///
+	/// Параметры:
+	///
+	///	bucket - имя папки, в которую будет сохранён файл
+	///	File - файл для загрузки на сервер
+	///
+	/// ]]>
+	/// </remarks>
+	/// <response code="200">Ссылка на загруженный файл</response>
+	/// <response code="400">Некорректные входные данные</response>
+	/// <response code="401">Пользователь не авторизован или авторизационный токен неверный</response>
 	[HttpPut(template: "{bucket}/upload")]
+	[Produces(contentType: MediaTypeNames.Application.Json)]
+	[ProducesResponseType(statusCode: StatusCodes.Status200OK, type: typeof(FileLink))]
+	[ProducesResponseType(statusCode: StatusCodes.Status400BadRequest, type: typeof(ErrorResponse))]
+	[ProducesResponseType(statusCode: StatusCodes.Status401Unauthorized, type: typeof(ErrorResponse))]
 	public async Task<ActionResult<FileLink>> UploadFile(
 		[FromRoute] string bucket,
 		[FromForm] FileRequest request,
@@ -70,7 +116,29 @@ public sealed class FileController(
 	#endregion
 
 	#region DELETE
+	/// <summary>
+	/// Удалить файл с сервера
+	/// </summary>
+	/// <remarks>
+	/// <![CDATA[
+	/// Пример запроса к API:
+	///
+	///	DELETE api/file/delete
+	///
+	/// Параметры:
+	///
+	///	Link - ссылка на файл для удаления с сервера
+	///
+	/// ]]>
+	/// </remarks>
+	/// <response code="200">Файл удален успешно</response>
+	/// <response code="400">Некорректные входные данные</response>
+	/// <response code="401">Пользователь не авторизован или авторизационный токен неверный</response>
 	[HttpDelete(template: "delete")]
+	[Produces(contentType: MediaTypeNames.Application.Json)]
+	[ProducesResponseType(statusCode: StatusCodes.Status200OK, type: typeof(void))]
+	[ProducesResponseType(statusCode: StatusCodes.Status400BadRequest, type: typeof(ErrorResponse))]
+	[ProducesResponseType(statusCode: StatusCodes.Status401Unauthorized, type: typeof(ErrorResponse))]
 	public async Task<ActionResult> DeleteFile(
 		[FromQuery] FileLink request,
 		CancellationToken cancellationToken = default(CancellationToken)
