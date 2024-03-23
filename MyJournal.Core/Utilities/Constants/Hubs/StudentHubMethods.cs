13cc7cb798c7d7880dba3c90b02554462de801ef
null <lesha.smirnov2019@mail.ru>
2024-03-23 22:53:52 +0300

    [+] added string constants with methods of new Hubs

---
+++ MyJournal.Core/Utilities/Constants/Hubs/StudentHubMethods.cs
@@ -0,0 +1,8 @@
+namespace MyJournal.Core.Utilities.Constants.Hubs;
+
+internal static class StudentHubMethods
+{
+	internal const string HubEndpoint = "https://localhost:7267/hub/student";
+	internal const string CompletedTask = nameof(CompletedTask);
+	internal const string UncompletedTask = nameof(UncompletedTask);
+}
---
+++ MyJournal.Core/Utilities/Constants/Hubs/TeacherHubMethods.cs
@@ -0,0 +1,7 @@
+namespace MyJournal.Core.Utilities.Constants.Hubs;
+
+internal static class TeacherHubMethods
+{
+	internal const string HubEndpoint = "https://localhost:7267/hub/teacher";
+	internal const string CreatedTask = nameof(CreatedTask);
+}
