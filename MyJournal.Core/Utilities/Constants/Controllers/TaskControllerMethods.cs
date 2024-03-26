33dd4f59176a7395c4cffe99d836fc9f7be5d54d
null <lesha.smirnov2019@mail.ru>
2024-03-26 23:23:14 +0300

    [+/=] added new and changed old TaskController methods

--- MyJournal.Core/Utilities/Constants/Controllers/TaskControllerMethods.cs
+++ MyJournal.Core/Utilities/Constants/Controllers/TaskControllerMethods.cs
@@ -10,13 +10,15 @@ internal static class TaskControllerMethods
 
 	internal const string GetAssignedTasks = "task/assigned/get";
 	internal const string GetAllAssignedTasks = "task/assigned/get/all";
-	internal const string GetAssignedByClassTasks = "task/assigned/get";
+	internal const string GetAssignedTasksForChildren = "task/assigned/children/get";
 	internal const string GetAllAssignedTasksForChildren = "task/assigned/children/get/all";
 	internal const string GetCreatedTasks = "task/created/get";
 	internal const string GetAllCreatedTasks = "task/created/get/all";
 	internal const string CreateTask = "task/create";
 
-	internal static string GetAssignedTasksForChildren(int classId) => $"task/assigned/class/{classId}/get";
+	internal static string GetAssignedTaskById(int taskId) => $"task/assigned/get/{taskId}";
+	internal static string GetCreatedTaskById(int taskId) => $"task/created/get/{taskId}";
+	internal static string GetAssignedByClassTasks(int classId) => $"task/assigned/class/{classId}/get";
 	internal static string GetAllAssignedByClassTasks(int classId) => $"task/assigned/class/{classId}/get/all";
 	internal static string ChangeCompletionStatusForTask(int taskId, CompletionStatus completionStatus)
 		=> $"task/{taskId}/completion-status/change/{completionStatus}";
