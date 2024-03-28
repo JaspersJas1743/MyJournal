b0609e4b11baac72af71870f1e2172f1b4bdd61e
null <lesha.smirnov2019@mail.ru>
2024-03-28 15:53:28 +0300

    [+/=] now you can get a list of study periods for each class

--- MyJournal.Core/Utilities/Constants/Controllers/ClassControllerMethods.cs
+++ MyJournal.Core/Utilities/Constants/Controllers/ClassControllerMethods.cs
@@ -3,4 +3,6 @@ namespace MyJournal.Core.Utilities.Constants.Controllers;
 public static class ClassControllerMethods
 {
 	public const string GetClasses = "class/all/get";
+
+	public static string GetEducationPeriodsByClass(int classId) => $"class/{classId}/periods/get";
 }
