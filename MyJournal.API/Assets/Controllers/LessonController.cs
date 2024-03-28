5c885fefc28b6fa9666796540a10471d3aeb1664
null <lesha.smirnov2019@mail.ru>
2024-03-28 15:55:18 +0300

    [+] now you can get a list of academic disciplines for each academic period

--- MyJournal.API/Assets/Controllers/LessonController.cs
+++ MyJournal.API/Assets/Controllers/LessonController.cs
@@ -35,7 +35,7 @@ public sealed class LessonController(
 	///
 	/// ]]>
 	/// </remarks>
-	/// <response code="200">Список дисциплин, изучаемых в текущем году</response>
+	/// <response code="200">Список дисциплин, изучаемых в текущем учебном периоде</response>
 	/// <response code="401">Пользователь не авторизован или авторизационный токен неверный</response>
 	/// <response code="403">Роль пользователя не соотвествует роли Student</response>
 	[HttpGet(template: "studying/get")]
@@ -65,6 +65,55 @@ public sealed class LessonController(
 	}
 
 	/// <summary>
+	/// [Студент] Получение списка дисциплин, преподаваемых обучающемуся в указанный учебный период
+	/// </summary>
+	/// <remarks>
+	/// <![CDATA[
+	/// Пример запроса к API:
+	///
+	///	GET api/subjects/studying/period/{period:string}/get
+	///
+	/// Параметры:
+	///
+	///	period - период, за который необходимо получить список изучаемых дисциплин
+	///
+	/// ]]>
+	/// </remarks>
+	/// <response code="200">Список дисциплин, изучаемых в указанный учебный период</response>
+	/// <response code="401">Пользователь не авторизован или авторизационный токен неверный</response>
+	/// <response code="403">Роль пользователя не соотвествует роли Student</response>
+	[HttpGet(template: "studying/period/{period}/get")]
+	[Authorize(Policy = nameof(UserRoles.Student))]
+	[Produces(contentType: MediaTypeNames.Application.Json)]
+	[ProducesResponseType(statusCode: StatusCodes.Status200OK, type: typeof(IEnumerable<GetStudyingSubjectsResponse>))]
+	[ProducesResponseType(statusCode: StatusCodes.Status401Unauthorized, type: typeof(ErrorResponse))]
+	[ProducesResponseType(statusCode: StatusCodes.Status403Forbidden, type: typeof(ErrorResponse))]
+	public async Task<ActionResult<IEnumerable<GetStudyingSubjectsResponse>>> GetStudyingSubjectsByPeriod(
+		[FromRoute] string period,
+		CancellationToken cancellationToken = default(CancellationToken)
+	)
+	{
+		int userId = GetAuthorizedUserId();
+		IQueryable<GetStudyingSubjectsResponse> learnedSubjects = _context.Students.AsNoTracking().AsSplitQuery()
+			.Where(predicate: s => s.UserId == userId)
+			.SelectMany(selector: s => s.Class.EducationPeriodForClasses)
+			.Where(predicate: epfc => epfc.EducationPeriod.Period == period)
+			.SelectMany(selector: epfc => epfc.Lessons)
+			.SelectMany(selector: l => l.TeachersLessons.Where(tl => tl.LessonId == l.Id).Select(tl => new GetStudyingSubjectsResponse(
+				l.Id,
+				l.Name,
+				new Teacher(
+					tl.Teacher.Id,
+					tl.Teacher.User.Surname,
+					tl.Teacher.User.Name,
+					tl.Teacher.User.Patronymic
+				)
+			)));
+
+		return Ok(value: learnedSubjects);
+	}
+
+	/// <summary>
 	/// [Преподаватель] Получение списка преподаваемых дисциплин по классам в текущий учебный период
 	/// </summary>
 	/// <remarks>
@@ -75,7 +124,7 @@ public sealed class LessonController(
 	///
 	/// ]]>
 	/// </remarks>
-	/// <response code="200">Список преподаваемых дисциплин по классам в текущем году</response>
+	/// <response code="200">Список преподаваемых дисциплин по классам в текущем учебном периоде</response>
 	/// <response code="401">Пользователь не авторизован или авторизационный токен неверный</response>
 	/// <response code="403">Роль пользователя не соотвествует роли Teacher</response>
 	[HttpGet(template: "taught/get")]
@@ -104,6 +153,52 @@ public sealed class LessonController(
 	}
 
 	/// <summary>
+	/// [Преподаватель] Получение списка преподаваемых дисциплин по классам в указанный учебный период
+	/// </summary>
+	/// <remarks>
+	/// <![CDATA[
+	/// Пример запроса к API:
+	///
+	///	GET api/subjects/taught/period/{period}/get
+	///
+	/// Параметры:
+	///
+	///	period - период, за который необходимо получить список изучаемых дисциплин
+	///
+	/// ]]>
+	/// </remarks>
+	/// <response code="200">Список преподаваемых дисциплин по классам в указанный учебный период</response>
+	/// <response code="401">Пользователь не авторизован или авторизационный токен неверный</response>
+	/// <response code="403">Роль пользователя не соотвествует роли Teacher</response>
+	[HttpGet(template: "taught/period/{period}/get")]
+	[Authorize(Policy = nameof(UserRoles.Teacher))]
+	[Produces(contentType: MediaTypeNames.Application.Json)]
+	[ProducesResponseType(statusCode: StatusCodes.Status200OK, type: typeof(IEnumerable<GetTaughtSubjectsResponse>))]
+	[ProducesResponseType(statusCode: StatusCodes.Status401Unauthorized, type: typeof(ErrorResponse))]
+	[ProducesResponseType(statusCode: StatusCodes.Status403Forbidden, type: typeof(ErrorResponse))]
+	public async Task<ActionResult<IEnumerable<GetTaughtSubjectsResponse>>> GetTaughtSubjectsByPeriod(
+		[FromRoute] string period,
+		CancellationToken cancellationToken = default(CancellationToken)
+	)
+	{
+		int userId = GetAuthorizedUserId();
+		IQueryable<GetTaughtSubjectsResponse> taughtSubjects = _context.Teachers.AsNoTracking().AsSplitQuery()
+			.Where(predicate: t => t.UserId == userId)
+			.SelectMany(selector: t => t.TeachersLessons)
+			.Where(predicate: tl => tl.Classes.Any(c => c.EducationPeriodForClasses.Any(epfc => epfc.EducationPeriod.Period == period)))
+			.SelectMany(selector: tl => tl.Classes.Select(c => new GetTaughtSubjectsResponse(
+				tl.LessonId,
+				tl.Lesson.Name,
+				new Class(
+					c.Id,
+					c.Name
+				)
+			)));
+
+		return Ok(value: taughtSubjects);
+	}
+
+	/// <summary>
 	/// [Родитель] Получение списка дисциплин, преподаваемых подопечному в текущий учебный период
 	/// </summary>
 	/// <remarks>
@@ -114,7 +209,7 @@ public sealed class LessonController(
 	///
 	/// ]]>
 	/// </remarks>
-	/// <response code="200">Список дисциплин, преподаваемых подопечному в текущем году</response>
+	/// <response code="200">Список дисциплин, преподаваемых подопечному в текущем учебном периоде</response>
 	/// <response code="401">Пользователь не авторизован или авторизационный токен неверный</response>
 	/// <response code="403">Роль пользователя не соотвествует роли Parent</response>
 	[HttpGet(template: "children/studying/get")]
@@ -148,13 +243,64 @@ public sealed class LessonController(
 	}
 
 	/// <summary>
+	/// [Родитель] Получение списка дисциплин, преподаваемых подопечному в указанный учебный период
+	/// </summary>
+	/// <remarks>
+	/// <![CDATA[
+	/// Пример запроса к API:
+	///
+	///	GET api/subjects/children/studying/period/{period}/get
+	///
+	/// Параметры:
+	///
+	///	period - период, за который необходимо получить список изучаемых дисциплин
+	///
+	/// ]]>
+	/// </remarks>
+	/// <response code="200">Список дисциплин, преподаваемых подопечному в указанный учебный период</response>
+	/// <response code="401">Пользователь не авторизован или авторизационный токен неверный</response>
+	/// <response code="403">Роль пользователя не соотвествует роли Parent</response>
+	[HttpGet(template: "children/studying/period/{period}/get")]
+	[Authorize(Policy = nameof(UserRoles.Parent))]
+	[Produces(contentType: MediaTypeNames.Application.Json)]
+	[ProducesResponseType(statusCode: StatusCodes.Status200OK, type: typeof(IEnumerable<GetStudyingSubjectsResponse>))]
+	[ProducesResponseType(statusCode: StatusCodes.Status401Unauthorized, type: typeof(ErrorResponse))]
+	[ProducesResponseType(statusCode: StatusCodes.Status403Forbidden, type: typeof(ErrorResponse))]
+	public async Task<ActionResult<IEnumerable<GetStudyingSubjectsResponse>>> GetSubjectsStudiedByWardByPeriod(
+		[FromRoute] string period,
+		CancellationToken cancellationToken = default(CancellationToken)
+	)
+	{
+		int userId = GetAuthorizedUserId();
+		IQueryable<GetStudyingSubjectsResponse> learnedSubjects = _context.Parents.AsNoTracking().AsSplitQuery()
+			.Where(predicate: p => p.UserId == userId)
+			.SelectMany(selector: p => p.Children.Class.EducationPeriodForClasses)
+			.Where(predicate: epfc => epfc.EducationPeriod.Period == period)
+			.SelectMany(selector: epfc => epfc.Lessons)
+			.SelectMany(selector: l => l.TeachersLessons.Where(tl => tl.LessonId == l.Id)
+				.Select(tl => new GetStudyingSubjectsResponse(
+					l.Id,
+					l.Name,
+					new Teacher(
+						tl.Teacher.Id,
+						tl.Teacher.User.Surname,
+						tl.Teacher.User.Name,
+						tl.Teacher.User.Patronymic
+					)
+				)
+			));
+
+		return Ok(value: learnedSubjects);
+	}
+
+	/// <summary>
 	/// [Администратор] Получение списка дисциплин, преподаваемых указанному классу в текущий учебный период
 	/// </summary>
 	/// <remarks>
 	/// <![CDATA[
 	/// Пример запроса к API:
 	///
-	///	GET api/subjects/{classId}/get
+	///	GET api/subjects/class/{classId}/get
 	///
 	/// Параметры:
 	///
@@ -162,10 +308,10 @@ public sealed class LessonController(
 	///
 	/// ]]>
 	/// </remarks>
-	/// <response code="200">Список дисциплин, преподаваемых указанному классу в текущем году</response>
+	/// <response code="200">Список дисциплин, преподаваемых указанному классу в текущем учебном периоде</response>
 	/// <response code="401">Пользователь не авторизован или авторизационный токен неверный</response>
 	/// <response code="403">Роль пользователя не соотвествует роли Administrator</response>
-	[HttpGet(template: "{classId:int}/get")]
+	[HttpGet(template: "class/{classId:int}/get")]
 	[Authorize(Policy = nameof(UserRoles.Administrator))]
 	[Produces(contentType: MediaTypeNames.Application.Json)]
 	[ProducesResponseType(statusCode: StatusCodes.Status200OK, type: typeof(IEnumerable<GetStudyingSubjectsResponse>))]
@@ -176,13 +322,71 @@ public sealed class LessonController(
 		CancellationToken cancellationToken = default(CancellationToken)
 	)
 	{
+		DateOnly nowDate = DateOnly.FromDateTime(dateTime: DateTime.Now);
 		IQueryable<GetStudyingSubjectsResponse> studyingSubjects = _context.Classes.AsNoTracking().AsSplitQuery()
 			.Where(predicate: c => c.Id == classId)
 			.SelectMany(selector: c => c.TeachersLessons)
+			.Where(predicate: tl => tl.Classes.Any(c => c.EducationPeriodForClasses.Any(epfc =>
+				EF.Functions.DateDiffDay(epfc.EducationPeriod.StartDate, nowDate) >= 0 &&
+				EF.Functions.DateDiffDay(nowDate, epfc.EducationPeriod.EndDate) <= 0
+			))).Select(selector: tl => new GetStudyingSubjectsResponse(
+				tl.LessonId,
+				tl.Lesson.Name,
+				new Teacher(
+					tl.Teacher.Id,
+					tl.Teacher.User.Surname,
+					tl.Teacher.User.Name,
+					tl.Teacher.User.Patronymic
+				)
+			));
+
+		return Ok(value: studyingSubjects);
+	}
+
+	/// <summary>
+	/// [Администратор] Получение списка дисциплин, преподаваемых указанному классу в текущий учебный период
+	/// </summary>
+	/// <remarks>
+	/// <![CDATA[
+	/// Пример запроса к API:
+	///
+	///	GET api/subjects/class/{classId:int}/period/{period}/get
+	///
+	/// Параметры:
+	///
+	///	classId - идентификатор класса, список дисциплин которого необходимо получить
+	///	period - период, за который необходимо получить список изучаемых дисциплин
+	///
+	/// ]]>
+	/// </remarks>
+	/// <response code="200">Список дисциплин, преподаваемых указанному классу в текущем учебном периоде</response>
+	/// <response code="401">Пользователь не авторизован или авторизационный токен неверный</response>
+	/// <response code="403">Роль пользователя не соотвествует роли Administrator</response>
+	[HttpGet(template: "class/{classId:int}/period/{period}/get")]
+	[Authorize(Policy = nameof(UserRoles.Administrator))]
+	[Produces(contentType: MediaTypeNames.Application.Json)]
+	[ProducesResponseType(statusCode: StatusCodes.Status200OK, type: typeof(IEnumerable<GetStudyingSubjectsResponse>))]
+	[ProducesResponseType(statusCode: StatusCodes.Status401Unauthorized, type: typeof(ErrorResponse))]
+	[ProducesResponseType(statusCode: StatusCodes.Status403Forbidden, type: typeof(ErrorResponse))]
+	public async Task<ActionResult<IEnumerable<GetStudyingSubjectsResponse>>> GetSubjectsStudiedInClassByPeriod(
+		[FromRoute] int classId,
+		[FromRoute] string period,
+		CancellationToken cancellationToken = default(CancellationToken)
+	)
+	{
+		IQueryable<GetStudyingSubjectsResponse> studyingSubjects = _context.Classes.AsNoTracking().AsSplitQuery()
+			.Where(predicate: c => c.Id == classId)
+			.SelectMany(selector: c => c.TeachersLessons)
+			.Where(predicate: tl => tl.Classes.Any(c => c.EducationPeriodForClasses.Any(epfc => epfc.EducationPeriod.Period == period)))
 			.Select(selector: tl => new GetStudyingSubjectsResponse(
 				tl.LessonId,
 				tl.Lesson.Name,
-				new Teacher(tl.Teacher.Id, tl.Teacher.User.Surname, tl.Teacher.User.Name, tl.Teacher.User.Patronymic)
+				new Teacher(
+					tl.Teacher.Id,
+					tl.Teacher.User.Surname,
+					tl.Teacher.User.Name,
+					tl.Teacher.User.Patronymic
+				)
 			));
 
 		return Ok(value: studyingSubjects);
--- MyJournal.Core/Utilities/Constants/Controllers/LessonControllerMethods.cs
+++ MyJournal.Core/Utilities/Constants/Controllers/LessonControllerMethods.cs
@@ -6,5 +6,9 @@ public static class LessonControllerMethods
 	public const string GetTaughtSubjects = "subjects/taught/get";
 	public const string GetSubjectsStudiedByWard = "subjects/children/studying/get";
 
-	public static string GetSubjectsStudiedInClass(int classId) => $"subjects/{classId}/get";
+	public static string GetStudyingSubjectsByPeriod(string period) => $"subjects/studying/period/{period}/get";
+	public static string GetTaughtSubjectsByPeriod(string period) => $"subjects/taught/period/{period}/get";
+	public static string GetSubjectsStudiedByWardByPeriod(string period) => $"subjects/children/studying/period/{period}/get";
+	public static string GetSubjectsStudiedInClassByPeriod(string period, int classId) => $"class/{classId:int}/period/{period}/get";
+	public static string GetSubjectsStudiedInClass(int classId) => $"subjects/class/{classId}/get";
 }
