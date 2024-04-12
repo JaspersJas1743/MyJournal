43b5a8e60d5350726591ea3006a9ac24a45a96c5
null <lesha.smirnov2019@mail.ru>
2024-04-12 13:45:15 +0300

    [+] a desktop project has been created for the My Journal application on AvaloniaUI

---
+++ MyJournal.Desktop/App.axaml.cs
@@ -0,0 +1,25 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using MyJournal.Desktop.ViewModels;
+using MyJournal.Desktop.Views;
+
+namespace MyJournal.Desktop;
+
+public partial class App : Application
+{
+	public override void Initialize()
+	{
+		AvaloniaXamlLoader.Load(this);
+	}
+
+	public override void OnFrameworkInitializationCompleted()
+	{
+		if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+		{
+			desktop.MainWindow = new MainWindow { DataContext = new MainWindowViewModel(), };
+		}
+
+		base.OnFrameworkInitializationCompleted();
+	}
+}
---
+++ MyJournal.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace MyJournal.Desktop.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
