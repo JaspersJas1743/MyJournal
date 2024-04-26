95c6a5a0173dda78d649c3f5396ddfc67eb0134b
null <lesha.smirnov2019@mail.ru>
2024-04-26 18:59:52 +0300

    [=] change System.Security.Authentication.AuthenticationException: The remote certificate is invalid according to the validation procedure: RemoteCertificateNameMismatch

--- MyJournal.API/Assets/Validation/PropertyValidationExtensions/ValidLinkExtensions.cs
+++ MyJournal.API/Assets/Validation/PropertyValidationExtensions/ValidLinkExtensions.cs
@@ -21,7 +21,12 @@ public static class ValidLinkExtensions
 
 	private static bool IsValidUrl(string? url)
 	{
-		using HttpClient client = new HttpClient();
+		HttpClientHandler clientHandler = new HttpClientHandler()
+		{
+			ServerCertificateCustomValidationCallback = (_, _, _, _) => true
+		};
+
+		using HttpClient client = new HttpClient(handler: clientHandler);
 		if (!Uri.TryCreate(uriString: url, uriKind: UriKind.Absolute, result: out Uri? uri))
 			return false;
 
