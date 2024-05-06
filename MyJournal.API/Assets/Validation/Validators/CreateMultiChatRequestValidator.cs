ed417f3ff9572ab7afe0818f62b69f1c439ed2fe
null <lesha.smirnov2019@mail.ru>
2024-05-06 15:14:42 +0300

    [=] fixed the error of rejecting requests for creating chats without a photo [=] fixed a bug where identifiers equal to 0 were allowed to create a multichat

--- MyJournal.API/Assets/Validation/Validators/CreateMultiChatRequestValidator.cs
+++ MyJournal.API/Assets/Validation/Validators/CreateMultiChatRequestValidator.cs
@@ -10,15 +10,18 @@ public sealed class CreateMultiChatRequestValidator : AbstractValidator<ChatCont
 	{
 		RuleFor(expression: request => request.InterlocutorIds)
 			.Cascade(cascadeMode: CascadeMode.Stop)
-			.AllElementsInCollection(predicate: Int32.IsPositive).WithMessage("Идентификатор собеседника должен быть больше 0.")
+			.AllElementsInCollection(predicate: element => element > 0).WithMessage("Идентификатор собеседника должен быть больше 0.")
 			.IsUniqueCollection().WithMessage("Идентификатор собеседника должен быть уникальным.");
 
 		RuleFor(expression: request => request.ChatName)
 			.HaveText(errorMessage: "Наименование чата имеет некорректный формат.");
 
-		RuleFor(expression: request => request.LinkToPhoto)
-			.HaveText(errorMessage: "Ссылка на фотографию имеет некорректный формат.")
-			.AllowFileExtensions(extensions: new string[] { "jpg", "png", "jpeg" })
-			.IsValidImageUrl().WithMessage(errorMessage: "Некорректная ссылка на изображение.");
+		When(predicate: request => !String.IsNullOrWhiteSpace(value: request.LinkToPhoto), action: () =>
+		{
+			RuleFor(expression: request => request.LinkToPhoto)
+				.HaveText(errorMessage: "Ссылка на фотографию имеет некорректный формат.")
+				.AllowFileExtensions(extensions: new string[] { "jpg", "png", "jpeg" })
+				.IsValidImageUrl().WithMessage(errorMessage: "Некорректная ссылка на изображение.");
+		});
 	}
 }
