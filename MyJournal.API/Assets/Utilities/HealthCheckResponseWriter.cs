b1a0151fc246439072dac362a721511703c95724
null <lesha.smirnov2019@mail.ru>
2024-05-07 23:30:54 +0300

    [=] fixed formatting of the response to the health request of the service

--- MyJournal.API/Assets/Utilities/HealthCheckResponseWriter.cs
+++ MyJournal.API/Assets/Utilities/HealthCheckResponseWriter.cs
@@ -13,15 +13,15 @@ public static class HealthCheckResponseWriter
 		using MemoryStream memoryStream = new MemoryStream();
 		await using Utf8JsonWriter jsonWriter = new Utf8JsonWriter(utf8Json: memoryStream, options: options);
 		jsonWriter.WriteStartObject();
-		jsonWriter.WriteString(propertyName: "status", value: healthReport.Status.ToString());
-		jsonWriter.WriteString(propertyName: "totalDuration", value: healthReport.TotalDuration.ToString());
-		jsonWriter.WriteStartObject(propertyName: "entries");
+		jsonWriter.WriteString(propertyName: "Status", value: healthReport.Status.ToString());
+		jsonWriter.WriteString(propertyName: "TotalDuration", value: healthReport.TotalDuration.ToString());
+		jsonWriter.WriteStartObject(propertyName: "Entries");
 
 		foreach (KeyValuePair<string, HealthReportEntry> healthReportEntry in healthReport.Entries)
 		{
 			jsonWriter.WriteStartObject(propertyName: healthReportEntry.Key);
 
-			jsonWriter.WriteStartObject(propertyName: "data");
+			jsonWriter.WriteStartObject(propertyName: "Data");
 			foreach (KeyValuePair<string, object> item in healthReportEntry.Value.Data)
 			{
 				jsonWriter.WritePropertyName(propertyName: item.Key);
@@ -29,11 +29,11 @@ public static class HealthCheckResponseWriter
 			}
 			jsonWriter.WriteEndObject();
 
-			jsonWriter.WriteString(propertyName: "duration", value: healthReportEntry.Value.Duration.ToString());
-			jsonWriter.WriteString(propertyName: "status", value: healthReportEntry.Value.Status.ToString());
-			jsonWriter.WriteString(propertyName: "description", value: healthReportEntry.Value.Description);
+			jsonWriter.WriteString(propertyName: "Duration", value: healthReportEntry.Value.Duration.ToString());
+			jsonWriter.WriteString(propertyName: "Status", value: healthReportEntry.Value.Status.ToString());
+			jsonWriter.WriteString(propertyName: "Description", value: healthReportEntry.Value.Description);
 
-			jsonWriter.WriteStartArray(propertyName: "tags");
+			jsonWriter.WriteStartArray(propertyName: "Tags");
 			foreach (string tag in healthReportEntry.Value.Tags)
 				jsonWriter.WriteStringValue(tag);
 			jsonWriter.WriteEndArray();
